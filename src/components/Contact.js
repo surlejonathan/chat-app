d12ac14222961e480544b49bdbc01f601b643f28
surlejonathan <surle.jonathan@gmail.com>
2020-10-10 11:20:02 +0200

    revert

---
+++ src/components/Contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../style/Contact.css';
+
+function Contact(prop) {
+  return (
+    <div className="Contact">
+        <img  className="avatar" src={prop.avatar} alt={prop.name}/>
+        <div>
+            <p className="name">{prop.name}</p>
+            <div className="status">
+                <span className={prop.online ? 'status-online' : 'status-offline'}/>
+                <p className="status-text">{prop.online ? 'online' : 'offline'}</p>
+            </div>
+        </div>
+       
+    </div>
+  );
+}
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired
+};
+export default Contact;
