cf0453d88feba7a37aca57f5690492c39bfa69c6
surlejonathan <surle.jonathan@gmail.com>
2020-10-12 13:38:17 +0200

    state - online/offline

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../style/Contact.css';
 
-function Contact(prop) {
-  return (
-    <div className="Contact">
-        <img  className="avatar" src={prop.avatar} alt={prop.name}/>
-        <div>
-            <p className="name">{prop.name}</p>
-            <div className="status">
-                <span className={prop.online ? 'status-online' : 'status-offline'}/>
-                <p className="status-text">{prop.online ? 'online' : 'offline'}</p>
-            </div>
-        </div>
-       
-    </div>
-  );
+class Contact extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      online: this.props.online
+    }
+  }
+
+  render () {
+    return (
+      <div className="Contact">
+          <img  className="avatar" src={this.props.avatar} alt={this.props.name}/>
+          <div>
+              <p className="name">{this.props.name}</p>
+              <div className="status">
+                  <span
+                  onClick ={
+                    (e) => {
+                    const offline = !this.state.online;
+                    this.setState({online : offline})
+                    }
+                  } 
+                  className={this.state.online ? 'status-online' : 'status-offline'}/>
+                  <p className="status-text">{this.state.online ? 'online' : 'offline'}</p>
+              </div>
+          </div>
+         
+      </div>
+    );
+  }
 }
 
 Contact.propTypes = {
